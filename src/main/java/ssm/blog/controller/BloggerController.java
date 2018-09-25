c4b856b6b8f6a6fc459416d2ff39f90bad8bf64e
Hquanquan <1432047930@qq.com>
2018-09-25 21:08:03 +0800

    blogtest

---
+++ src/main/java/ssm/blog/controller/BloggerController.java
@@ -0,0 +1,78 @@
+package ssm.blog.controller;
+
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ssm.blog.entity.Blogger;
+import ssm.blog.service.BloggerService;
+import ssm.blog.util.CryptographyUtil;
+
+@Controller
+@RequestMapping("/blogger")
+public class BloggerController {
+	
+	@Resource
+	private BloggerService bloggerService;
+	
+	@RequestMapping("/login")
+	public String login(Blogger blogger, HttpServletRequest request) {
+		//获取当前登陆的主体
+		Subject subject = SecurityUtils.getSubject(); 
+		//将密码使用md5加密
+		String newPassword = CryptographyUtil.md5(blogger.getPassword(), "javacoder");
+		//将用户信息封装到token中
+		UsernamePasswordToken token = new UsernamePasswordToken(blogger.getUsername(), newPassword);
+		try {
+			//会调用MyRealm中的doGetAuthenticationInfo方法进行身份认证,认证成功跳转到admin/main.jsp页面
+			subject.login(token); 
+			return "redirect:/admin/main.jsp";
+		} catch (AuthenticationException e) {
+			e.printStackTrace();
+			request.setAttribute("bloger", blogger);
+			request.setAttribute("errorInfo", "用户名或密码错误");
+			return "login";
+		} 
+
+	}
+	
+	@RequestMapping("/aboutme")
+	public ModelAndView abouotMe() {
+		ModelAndView modelAndView = new ModelAndView();
+		Blogger blogger = bloggerService.getBloggerData();
+		modelAndView.addObject("blogger", blogger);
+		modelAndView.addObject("commonPage", "foreground/blogger/bloggerInfo.jsp");
+		modelAndView.addObject("title", "个人博客");
+		modelAndView.setViewName("mainTemp");
+		return modelAndView;
+	}
+	
+	@RequestMapping("/myalbum")
+	public ModelAndView myAlbum() {
+		ModelAndView modelAndView = new ModelAndView();
+		//要写一个相册的service获取相册
+		//要建一个相册表
+		//....
+		modelAndView.addObject("commonPage", "foreground/blogger/myAlbum.jsp");
+		modelAndView.setViewName("mainTemp");
+		return modelAndView;
+	}
+	
+	@RequestMapping("/resource")
+	public ModelAndView resource() {
+		ModelAndView modelAndView = new ModelAndView();
+		//
+		//....
+		modelAndView.addObject("commonPage", "foreground/blogger/resource.jsp");
+		modelAndView.setViewName("mainTemp");
+		return modelAndView;
+	}
+}
